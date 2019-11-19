6be3c88376d21350ecc50cc48d6d8c32ebc0b54d
DuHuaL <469580396@qq.com>
2019-11-19 22:36:18 +0800

    state用法

--- src/components/coma.vue
+++ src/components/coma.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    {{ $store.state.count}}
+    {{num}}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 export default {
+  // 第一种
+  // computed: mapState(['count'])
+  // computed: mapState({
+  //   num: 'count'
+  // })
 
+  // 保留组件本身自己的计算属性
+  computed: {
+    // 计算属性本身的计算属性
+    // test() {},
+    ...mapState({
+      num: 'count'
+    })
+  }
 };
 </script>
 
